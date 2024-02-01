e889d78945cf0973f84e9dad1dc35a48ecd6447f
Wdowik2115 <116074280+Wdowik2115@users.noreply.github.com>
2024-02-01 12:22:33 +0100

    Add files via upload

---
+++ zbior.cpp
@@ -0,0 +1,125 @@
+﻿#include "zbior.h"
+#include <new>
+#include <random>
+
+void Zbior::alokuj(int n)
+{
+	if (n > 0)
+	{
+		m_elementy = new(std::nothrow) double[n];
+		m_liczbaElementow = n;
+	}
+}
+
+void Zbior::zwolnij()
+{
+	if (m_elementy != nullptr)
+	{
+		delete[] m_elementy;
+		m_elementy = nullptr;
+	}
+}
+
+Zbior::Zbior(size_t liczElem, double dGran, double gGran)
+{
+	alokuj(liczElem);
+	std::uniform_real_distribution<double> zakres(dGran, gGran);
+	std::random_device generator;
+	for (int elem = 0; elem < liczElem; elem++)
+		m_elementy[elem] = zakres(generator);
+}
+// Konstruktor przenoszacy
+Zbior::Zbior(Zbior&& other) 
+	: m_liczbaElementow(other.m_liczbaElementow),
+	m_elementy(other.m_elementy),
+	m_dolnaGranica(other.m_dolnaGranica),
+	m_gornaGranica(other.m_gornaGranica)
+{
+	other.m_liczbaElementow = 0;
+	other.m_elementy = nullptr;
+	other.m_dolnaGranica = 0;
+	other.m_gornaGranica = 0;
+
+}
+Zbior::Zbior(const Zbior& other)
+	: m_liczbaElementow(other.m_liczbaElementow),
+	m_dolnaGranica(other.m_dolnaGranica),
+	m_gornaGranica(other.m_gornaGranica)
+{
+	// Alokacja pamiêci
+	alokuj(m_liczbaElementow);
+	// Kopiowanie elementów
+	for (size_t i = 0; i < m_liczbaElementow; ++i)
+	{
+		m_elementy[i] = other.m_elementy[i];
+	}
+}
+// Operator przenoszenia 
+Zbior& Zbior::operator=(Zbior&& other)
+{
+	if (this != &other)
+	{
+		zwolnij(); // Zwolnienie aktualnych zasobów
+
+		// Przeniesienie pól
+		m_liczbaElementow = other.m_liczbaElementow;
+		m_elementy = other.m_elementy;
+		m_dolnaGranica = other.m_dolnaGranica;
+		m_gornaGranica = other.m_gornaGranica;
+
+		// Ustawienie pól obiektu zrodlowego na wartosci domyœlne
+		other.m_liczbaElementow = 0;
+		other.m_elementy = nullptr;
+		other.m_dolnaGranica = 0;
+		other.m_gornaGranica = 0;
+	}
+	return *this;
+}
+// Operator przypisania kopiujacego 
+Zbior& Zbior::operator=(const Zbior& other)
+{
+	if (this != &other)
+	{
+		zwolnij(); // Zwolnienie aktualnych zasobów
+
+		// Kopiowanie pól
+		m_liczbaElementow = other.m_liczbaElementow;
+		m_dolnaGranica = other.m_dolnaGranica;
+		m_gornaGranica = other.m_gornaGranica;
+
+		// Alokacja nowych zasobów
+		alokuj(m_liczbaElementow);
+
+		// Kopiowanie elementów
+		for (size_t i = 0; i < m_liczbaElementow; ++i)
+		{
+			m_elementy[i] = other.m_elementy[i];
+		}
+	}
+	return *this;
+}
+
+
+Zbior:: operator double() const
+{
+	if (m_liczbaElementow == 0)
+	{
+		return 0.0;
+	}
+	//Kopiowanie elementów do tymaczasowej tablicy
+	double* pomTab = new double[m_liczbaElementow];
+	std::copy(m_elementy, m_elementy + m_liczbaElementow, pomTab);
+	std::sort(pomTab, pomTab + m_liczbaElementow);
+	double mediana;
+	if (m_liczbaElementow % 2 == 0)
+	{
+		mediana = (pomTab[m_liczbaElementow / 2 - 1] + pomTab[m_liczbaElementow / 2]) / 2.0;
+	}
+	else
+	{
+		mediana = pomTab[m_liczbaElementow / 2];
+	}
+	//Zwolnienie pamieci
+	delete[] pomTab;
+	return mediana;
+}
---
+++ zbior.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+class Zbior
+{
+	size_t m_liczbaElementow = 0;
+	double* m_elementy = nullptr;
+	double m_dolnaGranica = 0;
+	double m_gornaGranica = 0;
+	void alokuj(int);
+	void zwolnij();
+public:
+	Zbior(size_t liczElem, double dGran, double gGran);
+	Zbior(Zbior&& other);
+	// Konstruktor kopiuj¹cy
+	Zbior(const Zbior& other);
+	// Operator przypisania kopiuj¹cego
+	Zbior& operator=(const Zbior& other);
+	operator double() const;
+	// Operator przypisania przenosz¹cego
+	Zbior& operator=(Zbior&& other);
+	double getGorna() const
+	{
+		return m_gornaGranica;
+	}
+	~Zbior() { zwolnij(); }
+
+	
+};
+
